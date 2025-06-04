f275b7082521eb63a5ec3a8d6adc7312f9f8acc7
danilojang <contatodanilojang@gmail.com>
2025-06-04 19:48:14 -0300

    Alteração da main para rota de app

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI
 from model.predict import prever_proximo_valor
+from pydantic import BaseModel
 
 app = FastAPI()
 
 class Entrada(BaseModel):
     fechamentos: list[float]
 
+@app.get("/")
+def home():
+    return {"status": "NVIDIA Stock Prediction API developed by Danilo Jang"}
+
 @app.post("/predict")
 def predict(entrada: Entrada):
     if len(entrada.fechamentos) < 60:
-        raise HTTPException(status_code=400, detail="Insira pelo menos 60 valores.")
-    try:
-        resultado = prever_proximo_valor(entrada.fechamentos)
-        return {"previsao": resultado}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        return {"error": "Insira pelo menos 60 valores."}
+    return {"previsao": prever_proximo_valor(entrada.fechamentos)}
